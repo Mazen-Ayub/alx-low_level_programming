9c7a78a853361aa0b04ce4cbb3277d43d1eb2c3d
Mazen-Ayub <mazenayub267@gmail.com>
2023-06-16 11:03:18 -0700

    ssign a random number to the variable n each time it is executed

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,15 +6,18 @@
  * Return : Always 0 (Success)
  */
 int main(void)
-{ int n;
-  srand(time(0));
-  n = rand() - RAND_MAX /2;
-  if (n == 0)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+if (n == 0)
 	printf("%d is zero\n", n);
-  else if (n < 0)
+else if (n < 0)
 	printf("%d is negative\n", n);
-  else
+else
 	printf("%d is positive\n", n);
 
-  return (0);
+return (0);
 }
