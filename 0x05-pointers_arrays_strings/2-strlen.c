5bb140ae3a84d287475e462fa3ae041317144fab
Mazen-Ayub <mazenayub267@gmail.com>
2023-06-26 13:46:52 -0700

    a function that returns the length of a string.

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen return the length of a string
+ *
+ * @s: the string that calculated
+ * Return: length of a the string.
+ */
+
+int _strlen(char *s)
+{
+	int x;
+
+	while (s[x] != '\0')
+	{
+		x++
+	}
+
+	return (x);
+}
