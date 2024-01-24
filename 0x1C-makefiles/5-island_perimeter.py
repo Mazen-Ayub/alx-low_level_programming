7d6e9281331b668e09faae20eab9bbac8a343209
Mazen Ayoub <105595088+Mazen-Ayub@users.noreply.github.com>
2024-01-24 16:02:08 +0200

    1

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module Perimeter
+"""
+
+
+def island_perimeter(grid):
+    """ Calculating the perimeter of grid"""
+    p = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            if grid[row][col] == 1:
+                if row == 0 or grid[row - 1][col] == 0:
+                    p += 1  # top
+                if row == (len(grid) - 1) or grid[row + 1][col] == 0:
+                    p += 1  # bottom
+                if col == 0 or grid[row][col - 1] == 0:
+                    p += 1  # left
+                if col == (len(grid[0]) - 1) or grid[row][col + 1] == 0:
+                    p += 1  # right
+    return p
+
